d9d6406972776e2f258e6b82a375c670cfc7cbfa
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-04-18 19:36:21 -0700

    updated data taken from react playground--no version control in playground

--- fetchingData.js
+++ fetchingData.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 
-const fetchingData = () => {
-  return (
-   /**
+/**
 1. import useEffect, useState
 2. add a post state variable with useState
-3. create a useEffect hook, inside that hook
+3. add the baseurl
+4. create a useEffect hook, inside that hook
       -create a fetch function async
       -create a response
       -transform the data into .json 
@@ -16,7 +16,39 @@ const fetchingData = () => {
       outside the function but inside of the hook, call the function
       -check if there are any dependencies
  */
-  )
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+export function App() {
+  const [posts, setPosts] = useState([]);
+  const [errors, setErrors] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${BASE_URL}/posts`); //you forgot the fetch!
+      const posts = await response.json();
+      console.log(posts);
+      setPosts(posts);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Fetch Posts</h1>
+      <h2>See all of the posts below!</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.title}
+            {post.body}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default fetchingData
+// Log to console
+console.log("Hello console");
+
+export default fetchingData;
