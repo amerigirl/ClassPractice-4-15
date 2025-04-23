73a464eec45f88669f8096c3fcb07ea1146a1b1c
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-04-22 18:46:38 -0700

    updated animal classes to be dynamic

--- BankAccount.py
+++ BankAccount.py
@@ -4,11 +4,22 @@ class Animal:
     #age
     #species
 
-    def __init__(self):
-        self.name = "Rose" 
-        self.age = 13
-        self.species = "Cat"
+#add dynamic attributes for properties
+    def __init__(self, name, age, species):
+        self.name = name 
+        self.age = age
+        self.species = species
 
-rose = Animal()
+rose = Animal("Rose", 5, "Dog")
+sparky = Animal("Sparky", 3, "Cat")
 print(rose)
+
+
 print(rose.name)
+print(rose.age)
+print(rose.species)
+
+print(sparky)
+print(sparky.name)
+print(sparky.age)
+print(sparky.species)
