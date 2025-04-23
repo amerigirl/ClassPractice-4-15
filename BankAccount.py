271d530ff7730b7bd5da9ebe4694a9bfe3c27e71
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-04-22 18:42:09 -0700

    creating init and adding arguments

--- BankAccount.py
+++ BankAccount.py
@@ -1,1 +1,14 @@
 #OOP -Classes - Blueprints for objects
+class Animal:
+    #name
+    #age
+    #species
+
+    def __init__(self):
+        self.name = "Rose" 
+        self.age = 13
+        self.species = "Cat"
+
+rose = Animal()
+print(rose)
+print(rose.name)
