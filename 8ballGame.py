2d50ea79724b1560e3489b33615e334644e3ef78
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-05-03 09:46:24 -0700

    having the user ask a question

--- 8ballGame.py
+++ 8ballGame.py
@@ -8,11 +8,30 @@ def print_menu():
     print("3. Type 'exit' to quit the game.")
     print()
 
+def show_help():
+    print("*************Help Menu*************")
+    print("In this game, you can ask any yes or no question.")
+    print("In this app, just enter a yes or no question to receive your fate!")
 
 
+def ask_question():
+    question = input("Ask your yes or no question: ").strip()
+    if question == "":
+        print("Uh-oh! You didn't ask a question. Please try again.")
+    else:
+        print()
+
+
+
+
+
+
+
+#runs game
 def run_game():
     print_menu()
 
 #Lets us run the file in the console and prevents the code from runing automatically if imported to antoher file
 if __name__ == "__main__":
     run_game()
+    show_help()
